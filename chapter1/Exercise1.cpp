6c446eab0334dde0a8f8a7fec5aeb4af4551ddf1
patelbramesh <patelbhramesh252@gmail.com>
2025-01-17 17:30:05 -0500

    first commit

---
+++ chapter1/Exercise1.cpp
@@ -0,0 +1,14 @@
+/*
+Programme file name: Exercise1.cpp
+Programmer name : Bramesh patel
+date : January 2025
+Requirements: 
+Write a program printing, hello, hello world.
+*/
+#include<iostream>
+
+int main(){
+
+std::cout <<"Hello World\n";
+
+}
