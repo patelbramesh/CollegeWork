099ec571d5035d4c7070872eee30b5b6389cfeff
patelbramesh <patelbhramesh252@gmail.com>
2025-02-03 08:30:03 -0500

    fourth commit

---
+++ chapter2/chapter2Exercise1A.cpp
@@ -0,0 +1,25 @@
+/*File name: chapter2Exercise1A
+Programmer: Bramesh Patel
+Date: January 2025
+Requirement:
+Write a program to find sum of 2 number;
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j,sum;
+
+    cout<<"Enter a number:";
+    cin>>i;
+
+    cout<<"Enter a number:";
+    cin>>j;
+
+    sum = i+j;
+
+    cout<<"Sum of two number is:"<<sum;
+
+    return 0;
+
+}
