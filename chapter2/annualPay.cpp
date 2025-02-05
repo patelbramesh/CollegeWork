1354b9e4842179b5cf9d95c51f3161e09cdec828
patelbramesh <patelbhramesh252@gmail.com>
2025-02-05 08:16:12 -0500

    5th commit

---
+++ chapter2/annualPay.cpp
@@ -0,0 +1,25 @@
+/* 
+File name: annualPay
+Programmer: Bramesh Patel
+Date: February 2025
+Requirement:
+Write a program for annual pay;
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    float payAmount,payPeriods,annualPay;
+
+    cout <<"Enter the amount you Earn:";
+    cin >>payAmount;
+
+    cout <<"Enter the number of pay period:";
+    cin >>payPeriods;
+
+    annualPay = payAmount * payPeriods;
+
+    cout <<"The amount earn per year is:"<<annualPay;
+}
