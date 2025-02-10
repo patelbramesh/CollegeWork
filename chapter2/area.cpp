c21af7cc09a23bb1b1365dbaae86934f84498b12
patelbramesh <patelbhramesh252@gmail.com>
2025-02-09 22:59:10 -0500

    8th branch commit

---
+++ chapter2/area.cpp
@@ -0,0 +1,31 @@
+/*
+File name: oceanLevel
+Programmer: Bramesh Patel
+Date: feburary 2025
+Requirements:
+Write a program for find area 
+*/
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main() {
+   
+     float height, length;
+     cout << "Height of the fence in feet:";
+     cin >> height;
+     cout << "length of the fence in feet:";
+     cin >> length;
+     float coveragePerGallon = 340.0;
+     int coats = 2;
+
+    float totalArea = height * length * coats;
+
+    float gallonsNeeded = totalArea / coveragePerGallon;
+
+    cout << "Total area to be painted: " << totalArea << " square feet ";
+    cout << fixed << setprecision(2) << "Gallons of paint needed: " << gallonsNeeded << endl;
+
+    return 0;
+}
