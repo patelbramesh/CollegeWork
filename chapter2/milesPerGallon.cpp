9b7920d0d5d4ab4bd5683b6c937b3e0bf233e01f
patelbramesh <patelbhramesh252@gmail.com>
2025-02-09 22:17:05 -0500

    7th commit

---
+++ chapter2/milesPerGallon.cpp
@@ -0,0 +1,22 @@
+/*
+File name: oceanLevel
+Programmer: Bramesh Patel
+Date: feburary 2025
+Requirements:
+Write a program for miles per gallon 
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float milesDriven = 375;
+    float gallonsUsed = 15;
+
+    float mpg = milesDriven / gallonsUsed;
+
+    cout << "The car's fuel efficiency is " << mpg << " miles per gallon.";
+
+    return 0;
+}
