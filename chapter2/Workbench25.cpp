d4bb4dd9b16cf7318ca8a1f16d11c4798bc504ac
patelbramesh <patelbhramesh252@gmail.com>
2025-02-12 08:27:07 -0500

    10th commit

---
+++ chapter2/Workbench25.cpp
@@ -0,0 +1,25 @@
+/*
+File name: Workbench25.cpp
+Programmer: Bramesh Patel
+Date: feburary 2025
+Requirements:
+a retail grants its customer a maximim amount of credit. Each customer's
+available credit is their maximum amount of credit minus the amount of credit used.
+
+wap an algorithm and then a c++ program that ask for a customer's maximum amout of credit,
+and amount of credit used. display the all information.
+
+1.Declare our variables.
+2.Get the varaiable information from the user
+3.calculate
+4. display input and output
+*/
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    double  avaiablecredit, creditUsed, maxCredit;
+
+    cout << ""
+}
