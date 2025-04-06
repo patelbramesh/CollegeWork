f9dee08907a32298768bd60550c554b606b59a92
patelbramesh <patelbhramesh252@gmail.com>
2025-04-06 10:02:55 -0400

    chapter5 1st commit

--- chapter5/chapter5part1exe17.cpp
+++ chapter5/chapter5part1exe17.cpp
@@ -1,55 +1,66 @@
 /*
 File name: chapter5part1exe17.cpp
-Programmer name: Bramesh patel
+Programmer name: Bramesh Patel
 Date: 03/25
 Requirements:
- write a program that can be used in math tutore for young students. That should display two random numbers 
- added. the program should pause till students work no the problem and when he or she press a key to check
- for correct answer.
+Write a program that can be used in math tutoring for young students. It should display two random numbers 
+to be added. The program should pause until the student works on the problem and presses a key to check 
+the correct answer. If it's correct, it should display a congratulatory message.
 */
-#include<iostream>
-#include<iomanip>
-#include<cstdlib>
-#include<ctime>
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-// Function to generate two random numbres
-int generateNumber(int &num1,int &num2){
-
-    
-    // generate random number between 100 to 999
-     num1 = rand ()% 900+100;
-     num2 = rand ()% 900+100;
-    
-    return 0;
+// Function to generate two random numbers
+void generateNumber(int &num1, int &num2) {
+    num1 = rand() % 900 + 100;
+    num2 = rand() % 900 + 100;
 }
-// Function to get result of two integers
-int getResult(int num1, int num2){
-    return num1 - num2;
+
+// Function to get the result of two integers
+int getResult(int num1, int num2) {
+    return num1 + num2; // This should be addition, not subtraction
 }
-int main()
-{
-    int num1,num2,userAnswer,correctAnswer;
-    
-    srand(time(0)); //this generate different number everytime
 
-    generateNumber(num1,num2);
-    correctAnswer = getResult(num1,num2);
-   
-    cout << "Solve the following problem:"<< endl;
-    cout << setw(5) << num1 << endl;
-    cout << setw(1) << "- "<< num2 << endl;
-    cout << setw(3) << "_____" << endl;
+// Function to ask if the user wants to repeat
+bool getRepeat() {
+    char choice;
+    cout << "Do you want to try another problem? (Y/N): ";
+    cin >> choice;
+    return (choice == 'Y' || choice == 'y');
+}
 
-    cout << "Enter your answer: "; //This where you need to write you calculated answer
-    cin >> userAnswer;
-    
-    //This will check if user has calculated correct or incorrect output
-    if(userAnswer == correctAnswer){
-        cout << "Answer is correct."<<endl;
-    }else{
-        cout << "Answer is Incorrect. Correct answer is "<< correctAnswer<< endl;
+int main() {
+    int num1, num2, userAnswer, correctAnswer;
+    srand(time(0)); // Seed the random number generator
+
+    bool repeat = true;
+
+    while (repeat) {
+        generateNumber(num1, num2);
+        correctAnswer = getResult(num1, num2);
+
+        cout << "\nSolve the following problem:\n";
+        cout << setw(5) << num1 << endl;
+        cout << "+ " << setw(3) << num2 << endl;
+        cout << "-----" << endl;
+
+        cout << "Enter your answer: ";
+        cin >> userAnswer;
+
+        if (userAnswer == correctAnswer) {
+            cout << "Congratulations! Your answer is correct 🎉." << endl;
+        } else {
+            cout << "Sorry, that's incorrect. The correct answer is " << correctAnswer << "." << endl;
+        }
+
+        repeat = getRepeat();
     }
+
+    cout << "Thank you for using the math tutor program. Goodbye!" << endl;
 
     return 0;
 }
---
+++ chapter5/chapter5part2exe11.cpp
@@ -0,0 +1,9 @@
+/*
+File name: chapter5part1exe17.cpp
+Programmer name: Bramesh patel
+Date: 03/25
+Requirements:
+ write a program that can be used in math tutore for young students. That should display two random numbers 
+ added. the program should pause till students work no the problem and when he or she press a key to check
+ for correct answer.
+*/
