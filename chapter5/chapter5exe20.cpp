c2f217b048b171bdeb3d59ff02b192a7f6216ae2
patelbramesh <patelbhramesh252@gmail.com>
2025-04-13 20:47:38 -0400

    chapter5 7th commit

---
+++ chapter5/chapter5exe20.cpp
@@ -0,0 +1,48 @@
+/*
+File Name: chapter5exe20.cpp
+Programmer Name: Bramesh Patel
+Date: 04/25
+Requirements: 
+Write a program to guess number. If guessed number is too high, display "Too high, guess again."
+If guessed number is too low, display "Too low, try again." Cycle keeps going until user guesses correctly.
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+// This will generate random number
+void generateRandomNumber(int &num) {
+    srand(time(0)); // Only call this once in main ideally
+    num = rand() % 50 + 1;
+}
+
+// Check guess and return true if correct
+bool checkNumber(int num, int guess) {
+    if (guess > num) {
+        cout << "Too High, guess again." << endl;
+        return false;
+    } else if (guess < num) {
+        cout << "Too low, try again." << endl;
+        return false;
+    } else {
+        cout << "Congratulations! You guessed the correct number." << endl;
+        return true;
+    }
+}
+
+int main() {
+    int num, guess;
+
+    generateRandomNumber(num);
+    cout << "Guess the number (between 1 and 50): ";
+
+    bool repeat = true;
+    while (repeat) {
+        cin >> guess;
+        repeat = !checkNumber(num, guess); // Keep looping until guess is correct
+    }
+
+    return 0;
+}
